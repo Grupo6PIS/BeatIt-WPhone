b95708034df7b252c282ca47bf3ac34e8fa9ca2c
MBerguer <popperismo@gmail.com>
2014-09-30 15:20:43 -0300

    Challenge 3

--- BeatIt!/AppCode/Pages/Challenge3.xaml.cs
+++ BeatIt!/AppCode/Pages/Challenge3.xaml.cs
@@ -8,7 +8,9 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using BeatIt_.AppCode.Interfaces;
 using Microsoft.Phone.Controls;
 
 /* INVITA A TUS AMIGOS */
@@ -22,14 +24,21 @@ namespace BeatIt_.AppCode.Pages
             InitializeComponent();
 
             NavigationInTransition navigateInTransition = new NavigationInTransition();
-            navigateInTransition.Backward = new SlideTransition { Mode = SlideTransitionMode.SlideRightFadeIn };
-            navigateInTransition.Forward = new SlideTransition { Mode = SlideTransitionMode.SlideLeftFadeIn };
+            navigateInTransition.Backward = new SlideTransition {Mode = SlideTransitionMode.SlideRightFadeIn};
+            navigateInTransition.Forward = new SlideTransition {Mode = SlideTransitionMode.SlideLeftFadeIn};
 
             NavigationOutTransition navigateOutTransition = new NavigationOutTransition();
-            navigateOutTransition.Backward = new SlideTransition { Mode = SlideTransitionMode.SlideRightFadeOut };
-            navigateOutTransition.Forward = new SlideTransition { Mode = SlideTransitionMode.SlideLeftFadeOut };
+            navigateOutTransition.Backward = new SlideTransition {Mode = SlideTransitionMode.SlideRightFadeOut};
+            navigateOutTransition.Forward = new SlideTransition {Mode = SlideTransitionMode.SlideLeftFadeOut};
             TransitionService.SetNavigationInTransition(this, navigateInTransition);
             TransitionService.SetNavigationOutTransition(this, navigateOutTransition);
+        }
+        private void hyperlinkButtonStartRunning_Click(object sender, RoutedEventArgs e)
+        {
+            this.StartPlayGrid.Visibility = Visibility.Collapsed;
+            this.InProgressGrid.Visibility = Visibility.Visible;
+
+            
         }
     }
 }
