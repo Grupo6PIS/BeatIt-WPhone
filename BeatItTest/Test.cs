c005555b7495e43faee73d392f50d57e929eb3b5
MBerguer <popperismo@gmail.com>
2014-10-01 20:54:06 -0300

    Primer test! UB

--- BeatItTest/Test.cs
+++ BeatItTest/Test.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using BeatIt_.AppCode.Challenges;
+using BeatIt_.AppCode.Classes;
+using BeatIt_.AppCode.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 //ojo
 
@@ -13,11 +16,18 @@ namespace BeatIt.Tests
             Assert.AreEqual(4 ,2+2);
             
             //IChallengeController ich = ChallengeController.getInstance();
-           // UsainBolt ubBolt = (UsainBolt)ich.getChallenge(BeatIt_.AppCode.Enums.ChallengeType.CHALLENGE_TYPE.USAIN_BOLT);
+            //UsainBolt ubBolt = (UsainBolt)ich.getChallenge(BeatIt_.AppCode.Enums.ChallengeType.CHALLENGE_TYPE.USAIN_BOLT);
 
-            // ACÁ se tiene que probar la función de puntaje, 
-            // el tema es que no está creada la función de almacenar el puntaje
-            //ubBolt.getPuntajeObtenido()
+            var ifc = FacadeController.getInstance();
+            if (!ifc.isLoggedUser())
+            {
+                ifc.loginUser(new User());
+            }
+            var currentChallenge = (ChallengeDetail1)ifc.getChallenge(1);
+            currentChallenge.completeChallenge(false, 45, 42);
+            //Si esta bien debería ser 174 el puntaje de en el desafío (42+45)*2
+            Assert.AreEqual(174, currentChallenge.State.getLastScore());
+
         }
     };
 };
