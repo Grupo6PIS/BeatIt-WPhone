d36aef8bf36c5d5a200af60c56adeffb106171c9
Martin Alayon <malayonoyarbide@gmail.com>
2014-10-18 14:03:15 -0200

    Desafio Song Complete (Logica)

--- BeatIt!/AppCode/Challenges/ChallengeDetail9.cs
+++ BeatIt!/AppCode/Challenges/ChallengeDetail9.cs
@@ -1,10 +1,13 @@
 ﻿using BeatIt_.AppCode.Classes;
+using BeatIt_.AppCode.Controllers;
 using BeatIt_.Resources;
 
 namespace BeatIt_.AppCode.Challenges
 {
     public class ChallengeDetail9 : Challenge
     {
+        public int TimerValue { get; set; }
+
         public ChallengeDetail9(int challengeId, string colorHex, int level, int maxAttempts, bool isEnabled)
         {
             ChallengeId = challengeId;
@@ -14,6 +17,7 @@ namespace BeatIt_.AppCode.Challenges
             Level = level;
             Description = level == 1 ? AppResources.Challenge9_DescriptionTxtBlockText : AppResources.Challenge9_DescriptionHardTxtBlockText;
             MaxAttempt = maxAttempts;
+            TimerValue = Level == 1 ? 20 : 10;
         }
 
         public ChallengeDetail9() 
@@ -25,6 +29,38 @@ namespace BeatIt_.AppCode.Challenges
             Level = 1;
             Description = AppResources.Challenge9_DescriptionTxtBlockText;
             MaxAttempt = 3;
+            TimerValue = 20;
+        }
+
+        private int CalculateScore(int[] miliseconds)
+        {
+            var res = 0;
+            for (int i = 0; i < miliseconds.Length; i++)
+            {
+                if (miliseconds[i] > 0)
+                    res = res + miliseconds[i];
+            }
+            return res;
+        }
+
+        public void CompleteChallenge(int[] miliseconds)
+        {
+            State.CurrentAttempt = State.CurrentAttempt + 1;
+
+            State.LastScore = CalculateScore(miliseconds);
+            if (State.LastScore > State.BestScore)
+            {
+                State.BestScore = State.LastScore;
+                FacadeController.GetInstance().ShouldSendScore = true;
+            }
+
+            if (State.CurrentAttempt == MaxAttempt)
+            {
+                State.Finished = true;
+            }
+
+            if (!FacadeController.GetInstance().GetIsForTesting())
+                FacadeController.GetInstance().SaveState(State);
         }
     }
 }
--- BeatIt!/AppCode/Pages/Challenge9.xaml.cs
+++ BeatIt!/AppCode/Pages/Challenge9.xaml.cs
@@ -1,9 +1,14 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 using System.Windows;
+using System.Windows.Media;
+using System.Windows.Threading;
 using BeatIt_.AppCode.Challenges;
 using BeatIt_.AppCode.Controllers;
 using BeatIt_.AppCode.Interfaces;
 using Microsoft.Phone.Controls;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 
 namespace BeatIt_.AppCode.Pages
 {
@@ -11,6 +16,9 @@ namespace BeatIt_.AppCode.Pages
     {
         private ChallengeDetail9 _currentChallenge;
         private IFacadeController _ifc;
+        private DispatcherTimer _timer;
+        private int _currentRound;
+        private int[] _result;
 
         public Challenge9()
         {
@@ -44,11 +52,135 @@ namespace BeatIt_.AppCode.Pages
                 .StartTime.ToString(CultureInfo.InvariantCulture);
             ToBeatTextBlock.Text = _currentChallenge.State.BestScore + " pts";
             DurationTextBlock.Text = _currentChallenge.GetDurationString();
+
+            _timer = new DispatcherTimer { Interval = new TimeSpan(0, 0, 1) };
+            _timer.Tick += TickTimer;
+
+            _result = new int[5];
+        }
+
+        private void NextStep(bool error)
+        {
+            bool songError;
+            _timer.Stop();
+            if (error)
+            {
+                songError = true;
+                _result[_currentRound] = 0;   
+            }
+            else
+            {
+                songError = false;
+                _result[_currentRound] = 50;
+            }
+            _currentRound++;
+            UpdateRectangle(_currentRound, songError);
+
+            if (_currentRound == 5)
+            {
+                _currentChallenge.CompleteChallenge(_result);
+                ToBeatTextBlock.Text = _currentChallenge.State.BestScore + " pts";
+
+                var uri = new Uri("/BeatIt!;component/AppCode/Pages/ChallengeDetail.xaml", UriKind.Relative);
+                NavigationService.Navigate(uri);
+
+                StartGrid.Visibility = Visibility.Visible;
+                InProgressGrid.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                ProgressBar.Value = 0;
+                ProgressBar.Minimum = 0;
+                ProgressBar.Maximum = _currentChallenge.TimerValue;
+
+                _timer.Start();
+
+                PlaySound("/BeatIt!;component/Sounds/dog_bark.wav");
+            }
+        }
+
+        private void TickTimer(object o, EventArgs e)
+        {
+            ProgressBar.Value = ProgressBar.Value + 1;
+
+            if ((int)ProgressBar.Value == _currentChallenge.TimerValue)
+            {
+                NextStep(true);
+            }
+        }
+
+        private void hyperlinkButtonStart_Click(object sender, RoutedEventArgs e)
+        {
+            StartGrid.Visibility = Visibility.Collapsed;
+            InProgressGrid.Visibility = Visibility.Visible;
+
+            _currentRound = 0;
+
+            ProgressBar.Value = 0;
+            ProgressBar.Minimum = 0;
+            ProgressBar.Maximum = _currentChallenge.TimerValue;
+
+            _timer.Start();
+
+            PlaySound("/BeatIt!;component/Sounds/dog_bark.wav");
         }
 
         private void image1_ImageFailed(object sender, ExceptionRoutedEventArgs e)
         {
 
+        }
+
+        private void Option1Button_Click(object sender, RoutedEventArgs e)
+        {
+            NextStep(false);
+        }
+
+        private void Option2Button_Click(object sender, RoutedEventArgs e)
+        {
+            NextStep(false);
+        }
+
+        private void Option3Button_Click(object sender, RoutedEventArgs e)
+        {
+            NextStep(false);
+        }
+
+        private void PlaySound(string path)
+        {
+            var stream = Application.GetResourceStream(new Uri(path, UriKind.Relative));
+            var soundeffect = SoundEffect.FromStream(stream.Stream);
+            var soundInstance = soundeffect.CreateInstance();
+            FrameworkDispatcher.Update();
+            soundInstance.Play();
+        }
+
+        private void UpdateRectangle(int round, bool error)
+        {
+
+            var mySolidColorBrush = new SolidColorBrush
+            {
+                Color = error
+                    ? System.Windows.Media.Color.FromArgb(255, 229, 20, 0)
+                    : System.Windows.Media.Color.FromArgb(255, 0, 138, 0)
+            };
+            switch (round)
+            {
+                case 1:
+                        Item1Rectangle.Fill = mySolidColorBrush;
+                    break;
+                case 2:
+                        Item2Rectangle.Fill = mySolidColorBrush;
+                    break;
+                case 3:
+                        Item3Rectangle.Fill = mySolidColorBrush;
+                    break;
+                case 4:
+                        Item4Rectangle.Fill = mySolidColorBrush;
+                    break;
+                case 5:
+                        Item5Rectangle.Fill = mySolidColorBrush;
+                    break;
+            }
         }
     }
 }
