53bdd083950f9983a260aff4e989409f525c20af
joaquin <joaquinvelazquezcamacho@gmail.com>
2014-10-19 18:18:10 -0200

    desafio 10, rest services

--- BeatIt!/AppCode/Pages/Challenge10.xaml.cs
+++ BeatIt!/AppCode/Pages/Challenge10.xaml.cs
@@ -1,9 +1,20 @@
 ﻿using Microsoft.Phone.Controls;
+using System.Net;
+using System;
+using Newtonsoft.Json.Linq;
+using System.Windows.Media.Imaging;
+using System.IO;
+using RestSharp;
+
+
 
 namespace BeatIt_.AppCode.Pages
 {
     public partial class Challenge10 : PhoneApplicationPage
     {
+
+        private string appKey = "mCRYcnQWo0mshpidnNRwHJqylB3Ap1crSMljsnExuNjkbguxty";
+
         public Challenge10()
         {
             InitializeComponent();
@@ -25,7 +36,41 @@ namespace BeatIt_.AppCode.Pages
 
         private void hyperlinkButtonStart_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-
+            countFacesServices(null);
         }
+
+        private void countFacesServices(byte[] image)
+        {
+            // HARDCODE >>>>>>
+
+            var bitImage = new BitmapImage();
+            bitImage.CreateOptions = BitmapCreateOptions.None;
+            bitImage.UriSource = new Uri("/BeatIt!;component/Images/Messi_Cara.png", UriKind.Relative);
+
+
+
+            WriteableBitmap writableBitmap = new WriteableBitmap(bitImage);
+            MemoryStream ms = new MemoryStream();
+            writableBitmap.SaveJpeg(ms, bitImage.PixelWidth, bitImage.PixelHeight, 0, 100);
+            byte[] imageBytes = ms.ToArray();
+
+            //HARDCODE  <<<<<<
+
+            var client = new RestClient("https://apicloud-facerect.p.mashape.com");
+            var request = new RestRequest("/process-file.json", Method.POST);
+
+            request.AddHeader("X-Mashape-Key", appKey);
+            request.AddFile("image", imageBytes, "image.png");
+
+            client.ExecuteAsync(request, response =>
+            {
+                if (response.ErrorMessage == null)
+                {
+                    var json = JObject.Parse(response.Content);
+                    int cantidad = ((JArray)(json["faces"])).Count;
+                }
+            });
+        }
+
     }
 }
